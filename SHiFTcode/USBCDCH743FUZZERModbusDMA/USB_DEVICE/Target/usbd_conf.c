ccc704a754ebff523e94308587084135437e43c8
Alejandro Mera <alejoseb@gmail.com>
2022-09-13 00:04:37 -0400

    templates and fuzzing examples added

--- SHiFTcode/USBCDCH743FUZZERModbusDMA/USB_DEVICE/Target/usbd_conf.c
+++ SHiFTcode/USBCDCH743FUZZERModbusDMA/USB_DEVICE/Target/usbd_conf.c
@@ -73,6 +73,7 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
   /* USER CODE BEGIN USB_OTG_FS_MspInit 0 */
 
   /* USER CODE END USB_OTG_FS_MspInit 0 */
+
   /** Initializes the peripherals clock
   */
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
@@ -81,6 +82,7 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
     {
       Error_Handler();
     }
+
   /** Enable USB Voltage detector
   */
     HAL_PWREx_EnableUSBVoltageDetector();
@@ -622,6 +624,22 @@ uint32_t USBD_LL_GetRxDataSize(USBD_HandleTypeDef *pdev, uint8_t ep_addr)
 {
   return HAL_PCD_EP_GetRxCount((PCD_HandleTypeDef*) pdev->pData, ep_addr);
 }
+
+#ifdef USBD_HS_TESTMODE_ENABLE
+/**
+  * @brief  Set High speed Test mode.
+  * @param  pdev: Device handle
+  * @param  testmode: test mode
+  * @retval USBD Status
+  */
+USBD_StatusTypeDef USBD_LL_SetTestMode(USBD_HandleTypeDef *pdev, uint8_t testmode)
+{
+  UNUSED(pdev);
+  UNUSED(testmode);
+
+  return USBD_OK;
+}
+#endif /* USBD_HS_TESTMODE_ENABLE */
 /**
   * @brief  Static single allocation.
   * @param  size: Size of allocated memory
@@ -682,5 +700,3 @@ USBD_StatusTypeDef USBD_Get_USB_Status(HAL_StatusTypeDef hal_status)
   }
   return usb_status;
 }
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ SHiFTcode/USBCDCH745FUZZERModbusDMA/CM7/Debug/FuzzerSHIFT/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU ARM embedded shadow 0x24000000
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../FuzzerSHIFT/src/MonitorAndTargetSingleCore.c 
+
+OBJS += \
+./FuzzerSHIFT/src/MonitorAndTargetSingleCore.o 
+
+C_DEPS += \
+./FuzzerSHIFT/src/MonitorAndTargetSingleCore.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+FuzzerSHIFT/src/%.o FuzzerSHIFT/src/%.su: ../FuzzerSHIFT/src/%.c FuzzerSHIFT/src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H745xx -c -I"/home/alejandro/Documents/semi-fuzz-project/SHiFT/SHiFTcode/USBCDCH745FUZZERModbusDMA/CM7/FuzzerSHIFT/Target/src/stm32f3_Modbus_Slave_UART-DMA-FreeRTOS/inc" -I../Core/Inc -I"/home/alejandro/Documents/semi-fuzz-project/SHiFT/SHiFTcode/USBCDCH745FUZZERModbusDMA/CM7/FuzzerSHIFT/Target/inc" -I"/home/alejandro/Documents/semi-fuzz-project/SHiFT/SHiFTcode/USBCDCH745FUZZERModbusDMA/CM7/FuzzerSHIFT/inc" -I../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -I../../../CommonSHIFT/Fuzzing/Inc -I../../../CommonSHIFT/FreeRTOS-Kernel/include -I../../../CommonSHIFT/FreeRTOS-Kernel/portable/GCC/ARM_CM4_MPU -I../../../CommonSHIFT/MallocMPU/FreeRTOS/ -I../../../CommonSHIFT/AsanRuntime/ASANMCU/inc -Ofast -ffunction-sections -fdata-sections -Wall -fno-common -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-FuzzerSHIFT-2f-src
+
+clean-FuzzerSHIFT-2f-src:
+	-$(RM) ./FuzzerSHIFT/src/MonitorAndTargetSingleCore.d ./FuzzerSHIFT/src/MonitorAndTargetSingleCore.o ./FuzzerSHIFT/src/MonitorAndTargetSingleCore.su
+
+.PHONY: clean-FuzzerSHIFT-2f-src
+
