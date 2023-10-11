ac774545bdda79e199a94e9ab58b3868e9e01d5e
Alejandro Mera <alejoseb@gmail.com>
2023-10-11 02:17:27 -0700

    template microchip completed

---
+++ SHiFTcode/TemplateSAMD51.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Default configuration
+CND_ARTIFACT_DIR_Default=dist/Default/production
+CND_ARTIFACT_NAME_Default=TemplateSAMD51.X.production.hex
+CND_ARTIFACT_PATH_Default=dist/Default/production/TemplateSAMD51.X.production.hex
