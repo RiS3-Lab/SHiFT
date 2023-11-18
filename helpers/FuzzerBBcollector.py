df5908322c3115324c36271596a5a5ad4b783614
Alejandro Mera <alejoseb@gmail.com>
2023-11-18 00:18:51 -0800

    bb collector improved, added cahe for bbs with huge speed up

--- helpers/FuzzerBBcollector.py
+++ helpers/FuzzerBBcollector.py
@@ -3,7 +3,7 @@
 
 import serial
 import subprocess, signal, time
-import argparse
+import argparse, os
 
 '''
  
@@ -48,36 +48,51 @@ if __name__ == "__main__":  # confirms that the code is under main function
     
     # set here the serialport to get the BBs
     #ser = serial.Serial('/dev/ttyACM0', baudrate=7500000, timeout = None)
-    ser = serial.Serial(args.serialBB, baudrate=args.baudBB, timeout = None)
+    ser = serial.Serial(args.serialBB, baudrate=args.baudBB, timeout = 10)
   
     procAFL = ''
     bbs={}
 
-    timestart = int(time.time()) 
+    timestart = int(time.time())
+    timeend = int(timestart + args.ttime)
+
+    print('Fuzzing for {} seconds'.format(args.ttime))
     
     
     try:
-      
+
+
+        os.makedirs(args.out, exist_ok=True) 
         # command to run AFL with proxy: set here the serialport for fuzztesting
         cmd_aflpp = ['../vanillaAFLplusplus/afl-fuzz', '-i', '../vanillaAFLplusplus/in', '-o', args.out ,'-t', '1000', '--','../shift_proxy/afl-proxy','-t', '1000', '-c', args.serial, '-w', str(args.baud) ]
         # launch AFL in a subprocess
         procAFL = subprocess.Popen(cmd_aflpp)
-                
-        while( int(time.time()) - timestart < args.ttime ):
-            line = str(ser.readline().decode())
-            if line[0] == '#':
-            #i-=1
+    
+      
+        while( True ):
+            if (int(time.time()) > timeend):
+                break
             
+            line = str(ser.readline().decode())
+            if len(line)==10 and  line[0] == '#':
+                     
                 bb ='0x'+line[1:9]
                 if bb not in bbs:
-                    bbs[bb] = int(time.time() - timestart) 
+                    t = int(time.time() - timestart)
+                    bbs[bb] = t
+                    with open(args.out + plot_data, 'a') as f:
+                        f.write('{} {} \n'.format(t,bb)) 
+                        f.close()
+
        
+        
+        ser.close()
         # terminate AFL
         procAFL.send_signal(signal.SIGINT)
         # wait till AFL terminates
         procAFL.wait()
         print(bbs)
-        saveBBs(bbs)
+        #saveBBs(bbs)
 
 
                       
@@ -88,8 +103,8 @@ if __name__ == "__main__":  # confirms that the code is under main function
         # wait till AFL terminates
         procAFL.wait()
         print(bbs)
-        saveBBs(bbs)
-        #ser.close()
+        #saveBBs(bbs)
+        ser.close()
         exit(0)
 
 
